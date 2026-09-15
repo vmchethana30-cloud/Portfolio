2b512e0cacf47349c89f87c9efe2d50721b30a01
Chethana Vadlamudi <vmchethana30@gmail.com>
2026-09-15 18:57:33 +1000

    Fix Contact Component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -47,9 +47,6 @@ export default function Contact() {
               chethana.vm@outlook.in
             </a>
           </div>
-
-          {/* Meta info */}
-          </div>
         </div>
       </div>
     </section>
